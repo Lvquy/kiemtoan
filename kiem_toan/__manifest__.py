2c3fc36d27088967d79f7986272bf2f3b1497c63
LvQuy <quyseo.ictu@gmail.com>
2023-02-10 10:39:40 +0700

    add module kiem_toan

---
+++ kiem_toan/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Trung tâm đào tạo Kiểm Toán Nhà Nước',
+    'version': '1',
+    'category': 'Trung tâm đào tạo kiểm toán',
+    'live_test_url': '#',
+    'summary': 'Phần mềm quản lý',
+    'author': 'Lv Quy',
+    'company': 'Trường Phát',
+    'website': 'https://#',
+    'depends': ['base_setup','website'],
+    'data': [
+        #data
+
+        # security
+        # 'security/groups.xml',
+        # 'security/ir.model.access.csv',
+        # report
+        # views
+        'views/custom_footer.xml',
+    ],
+
+    'assets': {
+        'web.assets_backend': [
+            # 'kiem_toan/static/src/css/style.css'
+        ],
+    },
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+}
